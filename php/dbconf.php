cce809d5143f1ec10682de5aa8b853ba0cf4a0ff
Nimal41 <Nimal2000thas17@gmail.com>
2024-12-07 05:58:35 +0000

    database conncted and register page linked with database

--- php/dbconf.php
+++ php/dbconf.php
@@ -1,16 +1,16 @@
 <?php
-define($HOST,'127.0.0.1:3306');
-define($User,'root');
-define($password,'mariadb');
-define($dbname,'atn');
+define('SERVERNAME', '127.0.0.1:3306');
+define('USERNAME', 'root');
+define('PASSWORD', 'mariadb');
+define('DBNAME', 'atn');
 
 try {
-    $connect=mysqli_connect($HOST,$User,$password,$dbname);
+    $connect=mysqli_connect(SERVERNAME,USERNAME,PASSWORD,DBNAME);
     if(!$connect){
         die("Database connection error!".mysqli_connect_error());
     }
     else{
-        echo "Database connected successfully!";
+        //echo "Database connected successfully!";
     }
 
     
