edbe38038f5be515ccdc76406f408988f9c77955
Nimal41 <Nimal2000thas17@gmail.com>
2025-01-02 12:39:17 +0000

    updated

---
+++ php/productdetails.php
@@ -0,0 +1,31 @@
+<?php
+	require_once 'dbconf.php';
+	function AddData($connect,$Productname,$Price,$Customer,$Customer_name,$Email,$Address){
+		try {
+           // $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+			$sql = "INSERT INTO orders VALUES('$Productname','$Price','$Customer','$Customer_name','$Email','$Address')";
+			$result = mysqli_query($connect,$sql);
+			if ($result) {
+				
+			} else {
+				die("Error ".mysqli_error($connect));
+			}
+            header('Location: ../productdetails.html');
+            exit;
+		} catch (Exception $e) {
+			die($e->getMessage());
+		}
+	}
+	
+	if ($_SERVER['REQUEST_METHOD'] == "POST") {
+		$Productname = $_POST['Productname'];
+		$Price= $_POST['Price'];
+		$Customer_name = $_POST['YourName'];
+        $Email= $_POST['Email'];
+        $Address= $_POST['Address'];
+
+
+		AddData($connect,$Productname,$Price,$Customer,$Customer_name,$Email,$Address);
+	}
+
+	?>
